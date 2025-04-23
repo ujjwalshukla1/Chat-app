3ac4999f8168d510e36d78465324ca6871ac739a
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 20:37:29 +0530

    instance update

--- Client/src/components/utilities/axiosinstance.js
+++ Client/src/components/utilities/axiosinstance.js
@@ -2,7 +2,7 @@
 import axios from "axios"; 
 
 const axiosInstance = axios.create({
-  baseURL : "http://localhost:10000/api/v1",
+  baseURL : "https://chat-backend-h0qf.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
