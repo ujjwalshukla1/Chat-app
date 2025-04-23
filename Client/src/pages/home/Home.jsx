26b562392cb94ec579c4fa44fe92940abada8445
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 21:37:44 +0530

    responsiveness added to home page

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,22 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "./SideBar";
 import MessageContainer from "./MessageContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeSocket, setOnlineUsers } from "../../store/slice/socket/socket.slice";
 import { setNewMessage } from "../../store/slice/message/Message.slice";
-
+import { FaBars } from "react-icons/fa"; 
 
 function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const { isAuthenticated, userProfile } = useSelector((state) => state.userSlice);
+  const { socket } = useSelector((state) => state.socketSlice);
+  const dispatch = useDispatch();
 
-  const {isAuthenticated} = useSelector((state) => state.userSlice);
-  const {userProfile} = useSelector((state) => state.userSlice);
-  const  dispatch = useDispatch();
-  const { socket } = useSelector((state) => state.socketSlice);  
- 
- 
   useEffect(() => {
     if (isAuthenticated) {
-     dispatch(initializeSocket(userProfile?._id));
+      dispatch(initializeSocket(userProfile?._id));
     }
   }, [isAuthenticated, dispatch, userProfile?._id]);
 
@@ -31,12 +29,31 @@ function Home() {
     return () => {
       socket.close();
     };
-  },[socket, dispatch]);
+  }, [socket, dispatch]);
 
   return (
-    <div className="flex">
-      <SideBar />
-      <MessageContainer />
+    <div className="flex h-screen overflow-hidden">
+      {/* Hamburger menu for mobile */}
+      <button
+        className="felx z-50 p-2 text-white rounded-md shadow md:hidden"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        <FaBars className="w-6 h-6 text-white" />
+      </button>
+
+      {/* Sidebar toggling */}
+      <div
+        className={`${
+          isSidebarOpen ? "block" : "hidden"
+        } md:block w-full md:max-w-[20em] z-40 text-white`}
+      >
+        <SideBar />
+      </div>
+
+      {/* Main message area */}
+      <div className="flex-1">
+        <MessageContainer />
+      </div>
     </div>
   );
 }
