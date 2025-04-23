2da985c22c5525c673e6860d81d0d43cd74e6896
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 19:54:46 +0530

    2

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/Store.js";
 import { Provider } from "react-redux";
-import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
