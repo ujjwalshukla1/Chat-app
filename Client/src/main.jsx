56fa878d234b6b51a6f9ee44199a6ef40988875f
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 19:57:20 +0530

    store.js modified

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import store from "./store/Store.js";
+import { BrowserRouter } from "react-router-dom"
+import { store } from "./store/Store.js";
 import { Provider } from "react-redux";
+
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
