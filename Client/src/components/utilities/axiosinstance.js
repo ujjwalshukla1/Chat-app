362354ad661175353c039775d3a36344a60ddc21
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 20:08:29 +0530

    instance

--- Client/src/components/utilities/axiosinstance.js
+++ Client/src/components/utilities/axiosinstance.js
@@ -2,7 +2,7 @@
 import axios from "axios"; 
 
 const axiosInstance = axios.create({
-  baseURL : "http://localhost:8000/api/v1",
+  baseURL : "http://localhost:10000/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
