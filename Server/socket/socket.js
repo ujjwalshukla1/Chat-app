f8c3e6f69b987f19b2f2fcb6063d210cac6dff02
Ujjwal shukla <shuklaujjwal6260@gmail.com>
2025-04-23 20:53:56 +0530

    socket updated

--- Server/socket/socket.js
+++ Server/socket/socket.js
@@ -1,39 +1,41 @@
-import {} from 'socket.io';
-import { Server } from 'socket.io';
-import http from 'http';
-import express from 'express';
+import {} from "socket.io";
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chat-backend-h0qf.onrender.com"]
+    origin: ["http://localhost:5173", "https://chat-backend-h0qf.onrender.com"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
 const userSocketMap = {
-    // userId: socketId
-}
+  // userId: socketId
+};
 
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    if (!userId) {
-        return;
-    }
+  const userId = socket.handshake.query.userId;
+  if (!userId) {
+    return;
+  }
 
-    userSocketMap[userId] = socket.id;
+  userSocketMap[userId] = socket.id;
+  io.emit("onlineUsers", Object.keys(userSocketMap));
+  console.log("User connected:", userId, socket.id);
+
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId];
     io.emit("onlineUsers", Object.keys(userSocketMap));
-    console.log("User connected:", userId, socket.id);
-
-    socket.on("disconnect", () => {
-        delete userSocketMap[userId];
-        io.emit("onlineUsers", Object.keys(userSocketMap));
-    });
-})
+  });
+});
 
 const getSocketId = (userId) => {
-    return userSocketMap[userId];
-}
+  return userSocketMap[userId];
+};
 
-export {io, app, server, getSocketId};
+export { io, app, server, getSocketId };
